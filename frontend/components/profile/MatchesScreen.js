6dd2fa57aae289c966668829179902cfac9815d7
Drake Morin <drake.lariviere@gmail.com>
2018-01-27 20:12:51 -0500

    Add View Files (#6)

---
+++ frontend/components/profile/MatchesScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import {
+  TextInput,
+  Button,
+  View,
+  Text
+} from 'react-native';
+import styles from '../../stylesheets/LoginStyles'
+
+export default class MatchesScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
