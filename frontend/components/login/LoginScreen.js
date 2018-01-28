952501fe0a9621bfb840c44c5bcbf9757a03120f
Drake Morin <drake.lariviere@gmail.com>
2018-01-28 06:11:11 -0500

    Check for empty password or email when logging in (#18)

--- frontend/components/login/LoginScreen.js
+++ frontend/components/login/LoginScreen.js
@@ -19,10 +19,20 @@ class LoginScreen extends React.Component {
     };
   }
 
+
+  displayAlert(title, message) {
+    Alert.alert(
+      title,
+      message,
+      [{text: 'OK', onPress: () => console.log('OK Pressed')},],
+      { cancelable: false }
+    )
+  }
   componentWillReceiveProps(nextProps) {
     if (nextProps.success) {
       this.props.changeView('Browse')
     }
+
   }
 
   onPressLogin() {
@@ -30,6 +40,13 @@ class LoginScreen extends React.Component {
     const { email, password } = this.state;
     console.log(email);
     console.log(password);
+    if (email === "") {
+      displayAlert("No Email", "Please enter your email");
+      return;
+    } else if (password === "") {
+      displayAlert("No Password", "Please enter your password");
+      return;
+    }
     let userData = {
       "email": email,
       "name": "Jacob Steves",
